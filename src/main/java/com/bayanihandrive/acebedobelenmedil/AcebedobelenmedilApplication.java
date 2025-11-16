cbfe8eebed2994c8fdb863c382c127352b6e92d4
Lord Anthone Acebedo <anthoneacebedo24@gmail.com>
2025-11-16 19:22:06 +0800

    Initial commit

---
+++ src/main/java/com/bayanihandrive/acebedobelenmedil/AcebedobelenmedilApplication.java
@@ -0,0 +1,13 @@
+package com.bayanihandrive.acebedobelenmedil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcebedobelenmedilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcebedobelenmedilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayanihandrive/acebedobelenmedil/AcebedobelenmedilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayanihandrive.acebedobelenmedil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcebedobelenmedilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
